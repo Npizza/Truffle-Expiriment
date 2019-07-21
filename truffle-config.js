4593cc6e77cdb415a2dc8b60ae812cbb96ece7af
Npizza <g0rg0s96@gmail.com>
2019-07-22 01:32:32 +0300

    uncomment the truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,12 +5,12 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  networks: {
-    development: {
-      host: "94.66.58.140",
-      port: 7545,
-      network_id: "*"
-    }
+  // networks: {
+  //   development: {
+  //     host: "94.66.58.140",
+  //     port: 7545,
+  //     network_id: "*"
+  //   }
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
